e4be2190a79a2e95a9016fb2e37954467b9194d4
xibei5678 <2668959317@qq.com>
2019-12-11 17:59:20 +0800

    更新内容

--- python_study/python_20191209_类与对象.py
+++ python_study/python_20191209_类与对象.py
@@ -9,36 +9,64 @@
 
 '''类'''
 
+'''
 # 类的语法
 # 1.关键字：class
 # 2.使用：
-#class 类名：   #类名的规范： 不能用关键字，驼峰命名StudPyhotn（首字母大写）
+#class 类名：   #类名的规范:不能用关键字，驼峰命名StudPyhotn（首字母大写）
   # 属性：变量
   # 行为：函数
+'''
 
 
 
-class SeniorTestingEngineer:  # 高级测试工程师类
-    # 属性
-    work_year = 3
-    salary = 20000
+# class SeniorTestingEngineer:  # 高级测试工程师类
+#     # 属性
+#     work_year = 3
+#     salary = 20000
+#
+#     # 行为
+#     def coding(self):  # self 对象本身--》有self标记为对象函数，只能对象来调用。self是变量，也可以用其他a，b等
+#         print('self:{}'.format(self))
+#         print('会写代码')
+#
+#     def do_sql(self):
+#         print('会操作数据库')
+#
+#     def do_linux(self):
+#         print('会使用linux系统')
+#
+#     def do_funtion_testing(self):
+#         print('会功能测试')
+#
+#     def do_api_test(self):
+#         print('会接口测试')
 
-    # 行为
-    def coding(self):  # self 对象本身--》对象函数，只能对象来调用
-        print('self:{}'.format(self))
-        print('会写代码')
-
-    def do_sql(self):
-        print('会操作数据库')
-
-    def do_linux(self):
-        print('会使用linux系统')
-
-    def do_funtion_testing(self):
-        print('会功能测试')
 
 
+
+
+'''
 # 1. 创建对象：类名（）
 # 2. 对象拥有类里面的所有属性和函数的使用权
 # 3. 类里面的函数只能有对象来调用
+# 4. 类里面的属性可以对象或类名调用
+'''
 
+
+# p_1 = SeniorTestingEngineer()  # 创建对象
+# # 属性和行为的调用
+# print(p_1.work_year)
+# print(p_1.salary)
+# p_1.coding()
+# p_1.do_funtion_testing()
+
+
+# p_2 = SeniorTestingEngineer  # p_2 不是对象
+# print(p_2.salary)  # 类名调用
+# print(p_2.work_year)  # 类名调用
+# # p_2.coding()  # 非对象调用，报错
+# # p_2.do_funtion_testing() # 非对象调用，报错
+#
+# # 非对象调用的时候 需传入对象
+# p_2.coding(SeniorTestingEngineer())
---
+++ python_study/python_20191210_类的调用.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+'''
+@File  : python_20191210_类的调用.py
+@Date  : 2019/12/10 0010 16:16
+@Author: xibei
+'''
+
+
+# class SeniorTestingEngineer:  # 高级测试工程师类
+#     # 属性
+#     work_year = 3
+#     salary = 20000
+#
+#     # 行为
+#     def coding(self):  # 对象方法
+#         print('self:{}'.format(self))
+#         print('会写代码')
+#         print(self.work_year)
+#
+#     @staticmethod
+#     def do_sql():  # 静态方法,没有self参数，可以非对象调用，类名.方法 进行调用，不需要特地创建对象调用
+#         print('会操作数据库')
+#         # print(self.work_year)  # 不支持属性调用，不使用类属性的方法可以写成静态方法
+#
+#     @classmethod  # 类方法，由cls参数标记
+#     def do_linux(cls):  # 类方法
+#         print('cls为：{}'.format(cls))
+#         print('会使用linux系统')
+#         print(cls.work_year)  # 类属性调用 ，不支持self.属性调用
+
+
+
+'''区别'''
+
+# 1.对象方法：有self参数，只能有对象调用
+#           对象方法中可直接通过self.属性 调用类的属性
+# 2.静态方法：由@staticmethod装饰器，无self参数，可以对象或类名调用
+#           静态方法中不可直接通过self.属性 调用类的属性
+# 3.类方法：由@classmethod装饰器，且有cls参数标记，可以对象或类名调用
+#           类方法中可直接通过cls.属性 调用类的属性
+# 4. 三种方法中都可以有：位置参数、默认参数、动态参数、关键字参数
+
+
+'''类的方法调用'''
+
+# p_1 = SeniorTestingEngineer()  # 创建对象
+# print(p_1.work_year)
+# print(p_1.salary)
+# p_1.coding()
+# p_1.do_sql()
+# p_1.do_linux()
+
+# P_2 = SeniorTestingEngineer  # 类名
+# print(P_2.work_year)  # 类名调用属性
+# print(P_2.salary)  # 类名调用属性
+# P_2.do_sql()  # 静态方法支持类名调用
+# P_2.do_linux()  # 类方法支持类名调用
+# # P_2.coding()  # 对象方法不支持类名调用
+
+
+'''类中的方法相互调用'''
+
+
+# class SeniorTestingEngineer:  # 高级测试工程师类
+#     # 属性
+#     work_year = 3
+#     salary = 20000
+#
+#     # 行为
+#     def coding(self):  # 对象方法
+#         print('会写代码')
+#
+#         # self.do_linux()  # 类方法调用
+#         # self.do_sql()  # 静态方法调用
+#
+#     @staticmethod
+#     def do_sql():
+#         print('会操作数据库')
+#         # SeniorTestingEngineer().coding()  # 对象方法调用
+#         # SeniorTestingEngineer().do_linux()  # 类方法调用
+#
+#
+#     @classmethod  # 类方法，由cls参数标记
+#     def do_linux(cls):  # 类方法
+#         print('会使用linux系统')
+#
+#         cls().coding()  # 对象方法调用
+#         cls.do_sql()  # 静态方法调用
+
+
+
+# p = SeniorTestingEngineer()  # 创建实例
+
+# p.coding()  # 测试 类方法和静态方法 调用
+
+# p.do_sql()  # 测试 对象方法和类方法 调用
+
+# p.do_linux()  # 测试 对象方法和静态方法 调用
