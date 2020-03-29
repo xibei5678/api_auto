7ec5a45c7bc72d69f945e96ee9a679a62f388c98
xibei5678 <2668959317@qq.com>
2020-03-29 21:03:11 +0800

    Default Changelist

---
+++ python_api_test_qianchendai/common/do_mysql.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/3/29 20:28
+# @Author : xibei
+# @filename : do_mysql.py
+# @description ：mysql 数据库连接
+
+import pymysql
+from python_api_test_qianchendai.common.read_conf import ReadConf
+
+
+
+class DoMysql:
+
+    def __init__(self):
+        host = ReadConf().get_conf_str("mysql", "host")
+        user = ReadConf().get_conf_str("mysql", "user")
+        password = ReadConf().get_conf_str("mysql", "password")
+        database = ReadConf().get_conf_str("mysql", "database")
+        port = ReadConf().get_conf_int("mysql", "port")
+        try:
+            self.cont = pymysql.connect(host=host, user=user, password=password, database=database, port=port)
+        except Exception as e:
+            raise e
+
+    def fecth_one(self, sql):  # 返回一条数据
+        curo = self.cont.cursor()  # 建立游标
+        curo.execute(sql)  # 执行sql
+        return curo.fetchone()  # 返回结果数据 元组形式
+
+
+if __name__ == '__main__':
+    sql = 'SELECT * FROM member WHERE MobilePhone !="" ORDER BY MobilePhone DESC LIMIT 1'
+    my_sql = DoMysql()
+    result = my_sql.fecth_one(sql)
+    print(result)
--- python_study/python_20200325_pymysql模块.py
+++ python_study/python_20200325_pymysql模块.py
@@ -14,21 +14,58 @@
 #         a.创建连接（连接成功与否）
 #         b.准备sql语句
 #         c.连接成功--建立游标
-#         d.执行
+#         d.执行sql
 #         e.返回结果
 
 
 import pymysql
 
-host="test.lemonban.com"
-user="test"
-password="test"
-database=None
-port=3306
+
 
 class DoMysql:
 
-    def __init__(self):
+    def __init__(self, host, user, password, database, port):
         try:
-            cont = pymysql.connect(host, user, password, database, port)
+            self.cont = pymysql.connect(host=host, user=user, password=password, database=database, port=port)
         except Exception as e:
+            raise e
+
+    def fecth_one(self, sql):  # 数据库查询 返回一条数据
+        curo = self.cont.cursor()  # 建立游标
+        curo.execute(sql)  # 执行sql
+        result = curo.fetchone()  # 返回结果数据 元组形式
+        curo.close()  # 关闭游标
+        return result
+
+
+    def fecth_all(self, sql):  # 数据库查询 返回所有数据
+        curo = self.cont.cursor()  # 建立游标
+        curo.execute(sql)  # 执行sql
+        result = curo.fetchall()  # 返回结果数据 元组形式
+        curo.close()
+        return result
+
+    # def fecth_many(self, sql, size):  # 数据库查询 返回多条数据 size:返回条数
+    #         curo = self.cont.cursor()  # 建立游标
+    #         curo.execute(sql)  # 执行sql
+    #         return curo.fetchmany(sql, size=size)  # 返回结果数据 元组形式
+
+    def db_close(self):
+        return self.cont.close()  # 关闭数据连接
+
+if __name__ == '__main__':
+
+    host = "test.lemonban.com"
+    user = "test"
+    password = "test"
+    database = "future"
+    port = 3306
+    sql = 'SELECT MobilePhone FROM member WHERE MobilePhone !="" ORDER BY MobilePhone DESC LIMIT 10'
+    # my_sql = pymysql.connect(host, user, password, database, port)
+    # cursor = my_sql.cursor()
+    # cursor.execute(sql)
+    # result = cursor.fetchone()  # 返回结果为元组
+    # print(result)
+    mysql = DoMysql(host, user, password, database, port)
+    res = mysql.fecth_all(sql=sql)
+    print(res)
